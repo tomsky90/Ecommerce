5248a937856b6e32957abe18cf3fdc19914474e9
Tomasz Kaszycki <kaszyckitomasz@interia.pl>
2023-12-28 03:13:03 +0000

    feat: create card component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,12 @@
 import Slider from "../../components/Slider/Slider";
+import FeaturedProducts from "../../components/FeaturedProducts/FeaturedProducts";
 import "./home.scss";
 const Home = () => {
   return (
     <div className="home">
       <Slider />
+      <FeaturedProducts type={"featured"} />
+      <FeaturedProducts type={"trending"} />
     </div>
   );
 };
