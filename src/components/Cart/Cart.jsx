590eb1a9266bf8a37181379fc4312b719bb11765
Tomasz Kaszycki <kaszyckitomasz@interia.pl>
2024-01-28 05:03:58 +0000

    feat: implement strapi payment

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,12 +1,33 @@
+import React from "react";
 import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
 import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
 import { removeItem, resetCart } from "../../redux/cartReducer";
+import { useDispatch } from "react-redux";
+import { makeRequest } from "../../makeRequest";
+import { loadStripe } from "@stripe/stripe-js";
 import "./cart.scss";
 
 const Cart = () => {
   const dispatch = useDispatch();
   const products = useSelector((state) => state.cart.products);
+
+  const stripePromise = loadStripe(
+    "pk_test_51OdNxuINCGdR9J4EQ3OCAaH1ZtdTQyeukW6wqna4J4qyXOudFT1qhVTiOVET9SrcBmxlMvOvSiwjHtz8OF7ERxAQ00eaNPSM1r"
+  );
+
+  const handlePayment = async () => {
+    try {
+      const stripe = await stripePromise;
+      const res = await makeRequest.post("/orders", {
+        products,
+      });
+      await stripe.redirectToCheckout({
+        sessionId: res.data.stripeSession.id,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const totalPrice = () => {
     let total = 0;
@@ -41,7 +62,9 @@ const Cart = () => {
         <span>SUBTOTAL</span>
         <span>${totalPrice()}</span>
       </div>
-      <button className="cart__checkout-btn">PROCEED TO CHECKOUT</button>
+      <button className="cart__checkout-btn" onClick={handlePayment}>
+        PROCEED TO CHECKOUT
+      </button>
       <span
         className="cart__reset"
         onClick={() => {
--- src/makeRequest.js
+++ src/makeRequest.js
@@ -3,7 +3,6 @@ import axios from "axios";
 export const makeRequest = axios.create({
   baseURL: import.meta.env.VITE_REACT_APP_API_URL,
   headers: {
-    Authorization:
-     "bearer" + import.meta.env.VITE_REACT_APP_API_TOKEN,
-  }
-})
+    Authorization: "bearer" + import.meta.env.VITE_REACT_APP_API_TOKEN,
+  },
+});
